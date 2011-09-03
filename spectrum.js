a6234f9be7adcfdc6597326e97bfd4ed496ee599
bgrins <briangrinstead@gmail.com>
2011-09-02 23:25:54 -0500

    allow setting picker from code

--- spectrum.js
+++ spectrum.js
@@ -16,6 +16,9 @@
         open: function() { },
 		flat: false
     };
+
+	var trimLeft = /^[\s,#]+/, 
+		trimRight = /\s+$/;
     
     function spectrum(element, o) {
 
@@ -39,27 +42,28 @@
 			currentX = 0,
 			currentY = 0,
 			currentHue = 0;
-        
-		var el;
-		if ($(element).is("input")) {
-			el = $(replaceInput);
-			$(element).hide().after(el);
+			
+        var boundElement = $(element);
+		var visibleElement;
+		if (boundElement.is("input")) {
+			visibleElement = $(replaceInput);
+			boundElement.hide().after(visibleElement);
 		}
 		else {
-			el = $(element);
+			visibleElement = $(element);
 		}
 		
-		el.addClass("spectrum-element");
+		visibleElement.addClass("spectrum-element");
 		
         /* Public API */
-        el.bind("spectrum.show", function() {
+        boundElement.bind("spectrum.show", function() {
 			if (visible) { return; }
 			visible = true;
 			
 			$(doc).bind("click", docClick);
 
-			var elOffset = el.offset();
-			elOffset.left += el.width();
+			var elOffset = visibleElement.offset();
+			elOffset.left += visibleElement.width();
             container.show().offset(elOffset);
             var off = dragger.offset();  
             offsetX = off.left;
@@ -70,7 +74,7 @@
             slideHeight = slider.height();
         });
 		
-        el.bind("spectrum.hide", function() {
+        boundElement.bind("spectrum.hide", function() {
 			if (!visible) { return; }
 			visible = false;
 			
@@ -78,31 +82,46 @@
             container.hide();    
         });
 		
-        el.bind("spectrum.set", function(e, c) {
-            dragger.css("background-color", c);
-			if (true) {
-			
-			}
+        boundElement.bind("spectrum.set", function(e, c) {
+            setColor(c);
         });
 		
-		el.click(function(e) {
-			$(this).trigger(visible ? "spectrum.hide" : "spectrum.show");
+		visibleElement.click(function(e) {
+			boundElement.trigger(visible ? "spectrum.hide" : "spectrum.show");
 			e.stopPropagation();
 		});
         
         /* DOM event handlers */
 		
 		function docClick() {
-			el.trigger("spectrum.hide");
+			boundElement.trigger("spectrum.hide");
 		}
 		
 		function move() {
 			var h = getCurrentHue();
 			var s = getCurrentSaturation();
             var v = getCurrentValue();
-			
+            
             var c = hsv2rgb(h, s, v);
+            
+            var dragHelperHeight = dragHelper.height() / 2;
+            dragHelper.css({
+                "top": currentY - dragHelperHeight,
+                "left": currentX - dragHelperHeight
+            });
 			
+			
+			var hex = hsv2rgb(h, 1, 1).hex;
+			console.log("HEX", hex)
+			
+			dragger.css("background-color", hex);
+			
+            var slideHelperHeight = slideHelper.height();
+            slideHelper.css({
+                "top": currentHue - (slideHelperHeight / 2)
+            });
+			
+
 			$("#console").css('background-color', c.hex);
 		}
 		
@@ -115,56 +134,125 @@
 		draggable(dragger, drag);
         
         function drag(e) {
-            var h = dragHelper.height();
 			currentX = e.dragX;
 			currentY = e.dragY;
-			
-            dragHelper.css({
-                "top": currentY - (h / 2),
-                "left": currentX - (h / 2)
-            });
 			
             $("#console").text(currentX + " " + currentY);
 			move();
         }
+        
+        function updateDragUI() {
+        
+        }
 		
         function slide(e) {
-            var h = slideHelper.height();
 			currentHue = e.dragY;
-            
-            slideHelper.css({
-                "top": currentHue - (h / 2)
-            });
-			
-			var c = hsv2rgb(getCurrentHue(), 1, 1);
-			dragger.css("background-color", c.hex);
 			move();
         }
 		
 		function getCurrentSaturation() {
 			return currentX / dragWidth;
 		}
+		
 		function getCurrentValue() {
 			return (dragHeight - currentY) / dragHeight
 		}
+		
 		function getCurrentHue() {
 			return (currentHue / slideHeight) * 360;
 		}
+		
+		
+		function setColor(color) {
+			var hsv;
+			console.log("SETTING COLOR", color)
+			if (typeof color == "object") {
+				if (color.hasOwnProperty("r")) {
+					hsv = rgb2hsv(color.r, color.g, color.b);
+				}
+				else if (color.hasOwnProperty("h")) {
+					hsv = color;
+				}
+			}
+			
+			if (typeof color == "string") {
+				
+    			color = color.replace(trimLeft,'').replace(trimRight, '').toLowerCase();
+    			
+    			if (htmlcodes[color]) {
+    				color = htmlcodes[color];
+    			}
+    			
+    			hsv = rgb2hsv(
+    				parseInt(color.substr(0, 2), 16), 
+    				parseInt(color.substr(2, 2), 16), 
+    				parseInt(color.substr(4, 2), 16)
+    			);
+			}
+			
+			var h = hsv.h % 360;
+	       	var s = hsv.s;
+	        var v = hsv.v;
+	        
+	        var c = hsv2rgb(h, s, v);
+	        
+			currentHue = (h * slideHeight) / 360;
+			currentX = s * dragWidth;
+	        currentY = dragHeight - (v * dragHeight);
+	        
+	        move();
+        }
         
         $(body).append(container.hide());
 		
 		if (opts.flat) {
-			el.trigger("spectrum.show");
+			boundElement.trigger("spectrum.show");
 		}
-        el.trigger("spectrum.set", opts.color);
+		
+        boundElement.trigger("spectrum.set", opts.color);
     }
 	
-    $.fn.spectrum = function(opts) {
-        return this.each(function() {
-            spectrum(this, opts);
-        }); 
-    };
+    function draggable(element, onmove, onstart, onstop) {
+		onmove = onmove || function() { };
+		onstart = onstart || function() { };
+		onstop = onstop || function() { };
+		var doc = element.ownerDocument || document;
+		var dragging = false;
+		var offset = { };
+		var maxHeight = 0;
+		var maxWidth = 0;
+            
+		function move(e) { 
+			if (dragging) {
+				e.dragX = Math.max(0, Math.min(e.pageX - offset.left, maxWidth));
+				e.dragY = Math.max(0, Math.min(e.pageY - offset.top, maxHeight));
+				
+				onmove.apply(element, [e]); 
+			} 
+		}
+		function start(e) { 
+			if (e.button == 0 && !dragging) { 
+				maxHeight = $(element).height();
+				maxWidth = $(element).width();
+				offset = $(element).offset();
+				$(doc).bind({ 
+					"mouseup": stop,
+					"mousemove": move
+				});
+				onstart.apply(element, arguments); 
+			} 
+			dragging = true; 
+		}
+		function stop() { 
+			if (dragging) { 
+				$(doc).unbind("mouseup", stop);
+				onstop.apply(element, arguments); 
+			}
+			dragging = false; 
+		}
 	
+		$(element).bind("mousedown", start);
+	}	
 	
     /**
      * Convert HSV representation to RGB HEX string.
@@ -210,49 +298,160 @@
         S = C == 0 ? 0 : C / V;
         return { h: H, s: S, v: V };
     }
-    
-    function draggable(element, onmove, onstart, onstop) {
-		onmove = onmove || function() { };
-		onstart = onstart || function() { };
-		onstop = onstop || function() { };
-		var doc = element.ownerDocument || document;
-		var dragging = false;
-		var offset = { };
-		var maxHeight = 0;
-		var maxWidth = 0;
-            
-		function move(e) { 
-			if (dragging) {
-				e.dragX = Math.max(0, Math.min(e.pageX - offset.left, maxWidth));
-				e.dragY = Math.max(0, Math.min(e.pageY - offset.top, maxHeight));
-				
-				onmove.apply(element, [e]); 
-			} 
-		}
-		function start(e) { 
-			if (e.button == 0 && !dragging) { 
-				maxHeight = $(element).height();
-				maxWidth = $(element).width();
-				offset = $(element).offset();
-				$(doc).bind({ 
-					"mouseup": stop,
-					"mousemove": move
-				});
-				onstart.apply(element, arguments); 
-			} 
-			dragging = true; 
-		}
-		function stop() { 
-			if (dragging) { 
-				$(doc).unbind("mouseup", stop);
-				onstop.apply(element, arguments); 
-			}
-			dragging = false; 
-		}
 	
-		$(element).bind("mousedown", start);
-	}	
+	var htmlcodes = {
+		aliceblue: 'f0f8ff',
+		antiquewhite: 'faebd7',
+		aqua: '00ffff',
+		aquamarine: '7fffd4',
+		azure: 'f0ffff',
+		beige: 'f5f5dc',
+		bisque: 'ffe4c4',
+		black: '000000',
+		blanchedalmond: 'ffebcd',
+		blue: '0000ff',
+		blueviolet: '8a2be2',
+		brown: 'a52a2a',
+		burlywood: 'deb887',
+		cadetblue: '5f9ea0',
+		chartreuse: '7fff00',
+		chocolate: 'd2691e',
+		coral: 'ff7f50',
+		cornflowerblue: '6495ed',
+		cornsilk: 'fff8dc',
+		crimson: 'dc143c',
+		cyan: '00ffff',
+		darkblue: '00008b',
+		darkcyan: '008b8b',
+		darkgoldenrod: 'b8860b',
+		darkgray: 'a9a9a9',
+		darkgreen: '006400',
+		darkkhaki: 'bdb76b',
+		darkmagenta: '8b008b',
+		darkolivegreen: '556b2f',
+		darkorange: 'ff8c00',
+		darkorchid: '9932cc',
+		darkred: '8b0000',
+		darksalmon: 'e9967a',
+		darkseagreen: '8fbc8f',
+		darkslateblue: '483d8b',
+		darkslategray: '2f4f4f',
+		darkturquoise: '00ced1',
+		darkviolet: '9400d3',
+		deeppink: 'ff1493',
+		deepskyblue: '00bfff',
+		dimgray: '696969',
+		dodgerblue: '1e90ff',
+		feldspar: 'd19275',
+		firebrick: 'b22222',
+		floralwhite: 'fffaf0',
+		forestgreen: '228b22',
+		fuchsia: 'ff00ff',
+		gainsboro: 'dcdcdc',
+		ghostwhite: 'f8f8ff',
+		gold: 'ffd700',
+		goldenrod: 'daa520',
+		gray: '808080',
+		grey: '808080',
+		green: '00ff00',
+		greenyellow: 'adff2f',
+		honeydew: 'f0fff0',
+		hotpink: 'ff69b4',
+		indianred : 'cd5c5c',
+		indigo : '4b0082',
+		ivory: 'fffff0',
+		khaki: 'f0e68c',
+		lavender: 'e6e6fa',
+		lavenderblush: 'fff0f5',
+		lawngreen: '7cfc00',
+		lemonchiffon: 'fffacd',
+		lightblue: 'add8e6',
+		lightcoral: 'f08080',
+		lightcyan: 'e0ffff',
+		lightgoldenrodyellow: 'fafad2',
+		lightgrey: 'd3d3d3',
+		lightgreen: '90ee90',
+		lightpink: 'ffb6c1',
+		lightsalmon: 'ffa07a',
+		lightseagreen: '20b2aa',
+		lightskyblue: '87cefa',
+		lightslateblue: '8470ff',
+		lightslategray: '778899',
+		lightsteelblue: 'b0c4de',
+		lightyellow: 'ffffe0',
+		lime: '00ff00',
+		limegreen: '32cd32',
+		linen: 'faf0e6',
+		magenta: 'ff00ff',
+		maroon: '800000',
+		mediumaquamarine: '66cdaa',
+		mediumblue: '0000cd',
+		mediumorchid: 'ba55d3',
+		mediumpurple: '9370d8',
+		mediumseagreen: '3cb371',
+		mediumslateblue: '7b68ee',
+		mediumspringgreen: '00fa9a',
+		mediumturquoise: '48d1cc',
+		mediumvioletred: 'c71585',
+		midnightblue: '191970',
+		mintcream: 'f5fffa',
+		mistyrose: 'ffe4e1',
+		moccasin: 'ffe4b5',
+		navajowhite: 'ffdead',
+		navy: '000080',
+		oldlace: 'fdf5e6',
+		olive: '808000',
+		olivedrab: '6b8e23',
+		orange: 'ffa500',
+		orangered: 'ff4500',
+		orchid: 'da70d6',
+		palegoldenrod: 'eee8aa',
+		palegreen: '98fb98',
+		paleturquoise: 'afeeee',
+		palevioletred: 'd87093',
+		papayawhip: 'ffefd5',
+		peachpuff: 'ffdab9',
+		peru: 'cd853f',
+		pink: 'ffc0cb',
+		plum: 'dda0dd',
+		powderblue: 'b0e0e6',
+		purple: '800080',
+		red: 'ff0000',
+		rosybrown: 'bc8f8f',
+		royalblue: '4169e1',
+		saddlebrown: '8b4513',
+		salmon: 'fa8072',
+		sandybrown: 'f4a460',
+		seagreen: '2e8b57',
+		seashell: 'fff5ee',
+		sienna: 'a0522d',
+		silver: 'c0c0c0',
+		skyblue: '87ceeb',
+		slateblue: '6a5acd',
+		slategray: '708090',
+		snow: 'fffafa',
+		springgreen: '00ff7f',
+		steelblue: '4682b4',
+		tan: 'd2b48c',
+		teal: '008080',
+		thistle: 'd8bfd8',
+		tomato: 'ff6347',
+		turquoise: '40e0d0',
+		violet: 'ee82ee',
+		violetred: 'd02090',
+		wheat: 'f5deb3',
+		white: 'ffffff',
+		whitesmoke: 'f5f5f5',
+		yellow: 'ffff00',
+		yellowgreen: '9acd32'
+	};
     
+    
+    $.fn.spectrum = function(opts) {
+        return this.each(function() {
+            spectrum(this, opts);
+        }); 
+    };
     window.spectrum = spectrum;
 
 })();
