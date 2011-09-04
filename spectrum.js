3fffc05d89a87a36c6c99ceab287c5e394e1bf1e
bgrins <briangrinstead@gmail.com>
2011-09-03 22:37:35 -0500

    add tinycolor for more flexibility on input

--- spectrum.js
+++ spectrum.js
@@ -152,14 +152,13 @@ Requires: jQuery, spectrum.css
             });
 			
 			// Update dragger background color
-			var flatHex = hsv2rgb(h, 1, 1).hex;
-			dragger.css("background-color", flatHex);
+			var flatColor = tinycolor({ h: h, s: 1, v: 1});
+			dragger.css("background-color", flatColor.toHexCss());
 			
-			var realHSV = { h: h, s: s, v: v };
-			var realRGB = hsv2rgb(h, s, v);
-			visibleElement.css("background-color", realRGB.hex);
+			var realColor = tinycolor({ h: h, s: s, v: v });
+			visibleElement.css("background-color", realColor.toHexCss());
 			
-			opts.move({ hsv: realHSV, rgb: realRGB, hex: realRGB.hex });
+			opts.move({ hsv: realColor.toHsv(), rgb: realColor.toRgb(), hex: realColor.toHexCss() });
 		}
 		
 		
@@ -175,41 +174,12 @@ Requires: jQuery, spectrum.css
 		
 		
 		function setColor(color) {
-			var hsv;
-			
-			if (typeof color == "object") {
-				if (color.hasOwnProperty("r")) {
-					hsv = rgb2hsv(color.r, color.g, color.b);
-				}
-				else if (color.hasOwnProperty("h")) {
-					hsv = color;
-				}
-			}
-			
-			if (typeof color == "string") {
-				
-    			color = color.replace(trimLeft,'').replace(trimRight, '').toLowerCase();
-    			
-    			if (htmlcodes[color]) {
-    				color = htmlcodes[color];
-    			}
-    			
-    			hsv = rgb2hsv(
-    				parseInt(color.substr(0, 2), 16), 
-    				parseInt(color.substr(2, 2), 16), 
-    				parseInt(color.substr(4, 2), 16)
-    			);
-			}
-			
-			var h = hsv.h % 360;
-	       	var s = hsv.s;
-	        var v = hsv.v;
-	        
-	        var c = hsv2rgb(h, s, v);
-	        
-	        currentHue = h;
-			currentSaturation = s;
-			currentValue = v;
+			var newColor = tinycolor(color);
+			var newHsv = newColor.toHsv();
+			console.log(newHsv.h, color)
+	        currentHue = newHsv.h * 360;;
+			currentSaturation = newHsv.s;
+			currentValue = newHsv.v;
 			
 	        updateUI();
         }
@@ -308,50 +278,6 @@ Requires: jQuery, spectrum.css
 		$(element).bind("mousedown", start);
 	}	
 	
-    /**
-     * Convert HSV representation to RGB HEX string.
-     * Credits to http://www.raphaeljs.com
-     */
-    function hsv2rgb(h, s, v) {
-        var R, G, B, X, C;
-        h = (h % 360) / 60;
-            C = v * s;
-        X = C * (1 - Math.abs(h % 2 - 1));
-        R = G = B = v - C;
-
-        h = ~~h;
-        R += [C, X, 0, 0, X, C][h];
-        G += [X, C, C, X, 0, 0][h];
-        B += [0, 0, X, C, C, X][h];
-
-        var r = R * 255,
-            g = G * 255,
-            b = B * 255;
-        return { r: r, g: g, b: b, hex: "#" + (16777216 | b | (g << 8) | (r << 16)).toString(16).slice(1) };
-    }
-
-    /**
-     * Convert RGB representation to HSV.
-     * r, g, b can be either in <0,1> range or <0,255> range.
-     * Credits to http://www.raphaeljs.com
-     */
-    function rgb2hsv(r, g, b) {
-        if (r > 1 || g > 1 || b > 1) {
-            r /= 255;
-            g /= 255;
-            b /= 255;            
-        }
-        var H, S, V, C;
-        V = Math.max(r, g, b);
-        C = V - Math.min(r, g, b);
-        H = (C == 0 ? null :
-             V == r ? (g - b) / C :
-             V == g ? (b - r) / C + 2 :
-                      (r - g) / C + 4);
-        H = (H % 6) * 60;
-        S = C == 0 ? 0 : C / V;
-        return { h: H, s: S, v: V };
-    }
     
     /**
      * Extend a given object with all the properties in passed-in object(s)
@@ -367,154 +293,6 @@ Requires: jQuery, spectrum.css
 		}
 		return obj;
 	};
-	
-	var htmlcodes = {
-		aliceblue: 'f0f8ff',
-		antiquewhite: 'faebd7',
-		aqua: '00ffff',
-		aquamarine: '7fffd4',
-		azure: 'f0ffff',
-		beige: 'f5f5dc',
-		bisque: 'ffe4c4',
-		black: '000000',
-		blanchedalmond: 'ffebcd',
-		blue: '0000ff',
-		blueviolet: '8a2be2',
-		brown: 'a52a2a',
-		burlywood: 'deb887',
-		cadetblue: '5f9ea0',
-		chartreuse: '7fff00',
-		chocolate: 'd2691e',
-		coral: 'ff7f50',
-		cornflowerblue: '6495ed',
-		cornsilk: 'fff8dc',
-		crimson: 'dc143c',
-		cyan: '00ffff',
-		darkblue: '00008b',
-		darkcyan: '008b8b',
-		darkgoldenrod: 'b8860b',
-		darkgray: 'a9a9a9',
-		darkgreen: '006400',
-		darkkhaki: 'bdb76b',
-		darkmagenta: '8b008b',
-		darkolivegreen: '556b2f',
-		darkorange: 'ff8c00',
-		darkorchid: '9932cc',
-		darkred: '8b0000',
-		darksalmon: 'e9967a',
-		darkseagreen: '8fbc8f',
-		darkslateblue: '483d8b',
-		darkslategray: '2f4f4f',
-		darkturquoise: '00ced1',
-		darkviolet: '9400d3',
-		deeppink: 'ff1493',
-		deepskyblue: '00bfff',
-		dimgray: '696969',
-		dodgerblue: '1e90ff',
-		feldspar: 'd19275',
-		firebrick: 'b22222',
-		floralwhite: 'fffaf0',
-		forestgreen: '228b22',
-		fuchsia: 'ff00ff',
-		gainsboro: 'dcdcdc',
-		ghostwhite: 'f8f8ff',
-		gold: 'ffd700',
-		goldenrod: 'daa520',
-		gray: '808080',
-		grey: '808080',
-		green: '00ff00',
-		greenyellow: 'adff2f',
-		honeydew: 'f0fff0',
-		hotpink: 'ff69b4',
-		indianred : 'cd5c5c',
-		indigo : '4b0082',
-		ivory: 'fffff0',
-		khaki: 'f0e68c',
-		lavender: 'e6e6fa',
-		lavenderblush: 'fff0f5',
-		lawngreen: '7cfc00',
-		lemonchiffon: 'fffacd',
-		lightblue: 'add8e6',
-		lightcoral: 'f08080',
-		lightcyan: 'e0ffff',
-		lightgoldenrodyellow: 'fafad2',
-		lightgrey: 'd3d3d3',
-		lightgreen: '90ee90',
-		lightpink: 'ffb6c1',
-		lightsalmon: 'ffa07a',
-		lightseagreen: '20b2aa',
-		lightskyblue: '87cefa',
-		lightslateblue: '8470ff',
-		lightslategray: '778899',
-		lightsteelblue: 'b0c4de',
-		lightyellow: 'ffffe0',
-		lime: '00ff00',
-		limegreen: '32cd32',
-		linen: 'faf0e6',
-		magenta: 'ff00ff',
-		maroon: '800000',
-		mediumaquamarine: '66cdaa',
-		mediumblue: '0000cd',
-		mediumorchid: 'ba55d3',
-		mediumpurple: '9370d8',
-		mediumseagreen: '3cb371',
-		mediumslateblue: '7b68ee',
-		mediumspringgreen: '00fa9a',
-		mediumturquoise: '48d1cc',
-		mediumvioletred: 'c71585',
-		midnightblue: '191970',
-		mintcream: 'f5fffa',
-		mistyrose: 'ffe4e1',
-		moccasin: 'ffe4b5',
-		navajowhite: 'ffdead',
-		navy: '000080',
-		oldlace: 'fdf5e6',
-		olive: '808000',
-		olivedrab: '6b8e23',
-		orange: 'ffa500',
-		orangered: 'ff4500',
-		orchid: 'da70d6',
-		palegoldenrod: 'eee8aa',
-		palegreen: '98fb98',
-		paleturquoise: 'afeeee',
-		palevioletred: 'd87093',
-		papayawhip: 'ffefd5',
-		peachpuff: 'ffdab9',
-		peru: 'cd853f',
-		pink: 'ffc0cb',
-		plum: 'dda0dd',
-		powderblue: 'b0e0e6',
-		purple: '800080',
-		red: 'ff0000',
-		rosybrown: 'bc8f8f',
-		royalblue: '4169e1',
-		saddlebrown: '8b4513',
-		salmon: 'fa8072',
-		sandybrown: 'f4a460',
-		seagreen: '2e8b57',
-		seashell: 'fff5ee',
-		sienna: 'a0522d',
-		silver: 'c0c0c0',
-		skyblue: '87ceeb',
-		slateblue: '6a5acd',
-		slategray: '708090',
-		snow: 'fffafa',
-		springgreen: '00ff7f',
-		steelblue: '4682b4',
-		tan: 'd2b48c',
-		teal: '008080',
-		thistle: 'd8bfd8',
-		tomato: 'ff6347',
-		turquoise: '40e0d0',
-		violet: 'ee82ee',
-		violetred: 'd02090',
-		wheat: 'f5deb3',
-		white: 'ffffff',
-		whitesmoke: 'f5f5f5',
-		yellow: 'ffff00',
-		yellowgreen: '9acd32'
-	};
-    
     
     $.fn.spectrum = function(opts) {
         return this.each(function() {
@@ -522,7 +300,7 @@ Requires: jQuery, spectrum.css
         	$(this).bind({
         		"spectrum.show": spect.show,
         		"spectrum.hide": spect.hide,
-        		"spectrum.set": spect.set
+        		"spectrum.set":  function(e, color) { spect.set(color); }
         	});
         }); 
     };
@@ -531,3 +309,507 @@ Requires: jQuery, spectrum.css
 })();
 
 
+// TinyColor.js - https://github.com/bgrins/TinyColor - 2011 Brian Grinstead - v0.1
+
+var tinycolor = (function() {
+
+var tc = _tinycolor;
+tc.version = "0.1";
+tc.equals = function(color1, color2) {
+	return tc(color1).toHex() == tc(color2).toHex();
+}
+
+var trimLeft = /^[\s,#]+/, 
+	trimRight = /\s+$/,
+	tinyCounter = 0;
+
+function _tinycolor (color) {
+	
+	// If input is already a tinycolor, return itself
+	if (typeof color == "object" && color.hasOwnProperty("_tc_id")) {
+		return color;
+	}
+	
+	var rgb = inputToRGB(color);
+	var r = rgb.r, g = rgb.g, b = rgb.b;
+	
+	return {
+		_tc_id: tinyCounter++,
+		toHsv: function() {
+			return rgbToHsv(r, g, b);
+		},
+		toHsl: function() {
+			return rgbToHsl(r, g, b);
+		},
+		toHslCss: function() {
+			var hsl = rgbToHsl(r, g, b);
+			var h = Math.round(hsl.h * 360);
+			var s = Math.round(hsl.s * 100);
+			var l = Math.round(hsl.l * 100);
+			return "hsl(" + h + ", " + s + "%, " + l + "%)";
+		},
+		toHex: function() {
+			return rgbToHex(r, g, b);
+		},
+		toHexCss: function() {
+			return '#' + rgbToHex(r, g, b);
+		},
+		toRgb: function() {
+			return { r: r, g: g, b: b };
+		},
+		toRgbCss: function() {
+			return "rgb(" + r + ", " + g + ", " + b + ")";
+		},
+		toName: function() {
+			var hex = rgbToHex(r, g, b);
+			for (var i in htmlcodes) {
+				if (htmlcodes[i] == hex) {
+					return i;
+				}
+			}
+			return false;
+		}
+	};
+};
+
+
+function inputToRGB(color) {
+
+	var r = g = b = 255;
+	var ok = false;
+	
+	if (typeof color == "string") {
+    	color = color.replace(trimLeft,'').replace(trimRight, '').toLowerCase();
+    	if (htmlcodes[color]) {
+    		color = htmlcodes[color];
+    	}
+    	
+    	for (var i = 0; i < colorparsers.length; i++) {
+    	    var processor = colorparsers[i].process;
+    	    var bits = colorparsers[i].re.exec(color);
+    	    if (bits) {
+    	        channels = processor(bits);
+    	        r = channels[0];
+    	        g = channels[1];
+    	        b = channels[2];
+    	        ok = true;
+    	    }
+		
+    	}
+	}
+	else if (typeof color == "object") {
+		if (color.hasOwnProperty("r")) {
+			r = color.r;
+			g = color.g;
+			b = color.b;		
+		}
+		if (color.hasOwnProperty("h") && color.hasOwnProperty("s") && color.hasOwnProperty("v")) {
+			var rgb = hsvToRgb(color.h, color.s, color.v);
+			
+			r = rgb.r;
+			g = rgb.g;
+			b = rgb.b;
+		}
+		if (color.hasOwnProperty("h") && color.hasOwnProperty("s") && color.hasOwnProperty("l")) {
+			var rgb = hslToRgb(color.h, color.s, color.l);
+			r = rgb.r;
+			g = rgb.g;
+			b = rgb.b;
+		}
+	}
+	
+	return {
+		r: Math.min(255, Math.max(parseInt(r, 10), 0)),
+		g: Math.min(255, Math.max(parseInt(g, 10), 0)),
+		b: Math.min(255, Math.max(parseInt(b, 10), 0))
+	};
+}
+
+// hsv and hsl to and from rgb taken from: http://mjijackson.com/2008/02/rgb-to-hsl-and-rgb-to-hsv-color-model-conversion-algorithms-in-javascript
+/** 
+ * Converts an RGB color value to HSL. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+ * Assumes r, g, and b are contained in the set [0, 255] and
+ * returns h, s, and l in the set [0, 1].
+ *
+ * @param   Number  r       The red color value
+ * @param   Number  g       The green color value
+ * @param   Number  b       The blue color value
+ * @return  Array           The HSL representation
+ */
+function rgbToHsl(r, g, b){
+    r /= 255, g /= 255, b /= 255;
+    var max = Math.max(r, g, b), min = Math.min(r, g, b);
+    var h, s, l = (max + min) / 2;
+
+    if(max == min){
+        h = s = 0; // achromatic
+    }else{
+        var d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+        switch(max){
+            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+            case g: h = (b - r) / d + 2; break;
+            case b: h = (r - g) / d + 4; break;
+        }
+        h /= 6;
+    }
+
+    return { h: h, s: s, l: l };
+}
+
+function bound01(n, max) {
+	n = parseFloat(n);
+	if (n == max) {
+		return 1;
+	}
+	else if (n > 1) {
+		return (n % max) / parseFloat(max);
+	}
+	return n;
+}
+
+/**
+ * Converts an HSL color value to RGB. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+ * Assumes h, s, and l are contained in the set [0, 1] and
+ * returns r, g, and b in the set [0, 255].
+ *
+ * @param   Number  h       The hue
+ * @param   Number  s       The saturation
+ * @param   Number  l       The lightness
+ * @return  Array           The RGB representation
+ */
+function hslToRgb(h, s, l){
+    var r, g, b;
+
+	h = bound01(h, 360);
+	s = bound01(s, 100);
+	l = bound01(l, 100);
+	
+    function hue2rgb(p, q, t){
+        if(t < 0) t += 1;
+        if(t > 1) t -= 1;
+        if(t < 1/6) return p + (q - p) * 6 * t;
+        if(t < 1/2) return q;
+        if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+        return p;
+    }
+    
+    if(s == 0){
+        r = g = b = l; // achromatic
+    }else{
+
+        var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+        var p = 2 * l - q;
+        r = hue2rgb(p, q, h + 1/3);
+        g = hue2rgb(p, q, h);
+        b = hue2rgb(p, q, h - 1/3);
+    }
+
+    return { r: r * 255, g: g * 255, b: b * 255 };
+}
+
+/**
+ * Converts an RGB color value to HSV. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSV_color_space.
+ * Assumes r, g, and b are contained in the set [0, 255] and
+ * returns h, s, and v in the set [0, 1].
+ *
+ * @param   Number  r       The red color value
+ * @param   Number  g       The green color value
+ * @param   Number  b       The blue color value
+ * @return  Array           The HSV representation
+ */
+function rgbToHsv(r, g, b){
+    r = r/255, g = g/255, b = b/255;
+    var max = Math.max(r, g, b), min = Math.min(r, g, b);
+    var h, s, v = max;
+
+    var d = max - min;
+    s = max == 0 ? 0 : d / max;
+
+    if(max == min){
+        h = 0; // achromatic
+    }else{
+        switch(max){
+            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+            case g: h = (b - r) / d + 2; break;
+            case b: h = (r - g) / d + 4; break;
+        }
+        h /= 6;
+    }
+
+    return { h: h, s: s, v: v };
+}
+
+
+/**
+ * Converts an HSV color value to RGB. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSV_color_space.
+ * Assumes h, s, and v are contained in the set [0, 1] and
+ * returns r, g, and b in the set [0, 255].
+ *
+ * @param   Number  h       The hue
+ * @param   Number  s       The saturation
+ * @param   Number  v       The value
+ * @return  Array           The RGB representation
+ */
+function hsvToRgb(h, s, v){
+    var r, g, b;
+    
+    
+	h = bound01(h, 360);
+	s = bound01(s, 100);
+	v = bound01(v, 100);
+	
+    var i = Math.floor(h * 6);
+    var f = h * 6 - i;
+    var p = v * (1 - s);
+    var q = v * (1 - f * s);
+    var t = v * (1 - (1 - f) * s);
+    
+    switch(i % 6){
+        case 0: r = v, g = t, b = p; break;
+        case 1: r = q, g = v, b = p; break;
+        case 2: r = p, g = v, b = t; break;
+        case 3: r = p, g = q, b = v; break;
+        case 4: r = t, g = p, b = v; break;
+        case 5: r = v, g = p, b = q; break;
+    }
+    
+    return {r: r * 255, g: g * 255, b: b * 255};
+}
+
+
+function rgbToHex(r, g, b) {
+	function pad(c) {
+		return c.length == 1 ? '0' + c : c;
+	}	
+	return [ 
+		pad(r.toString(16)),
+		pad(g.toString(16)),
+		pad(b.toString(16))
+	].join("");
+}
+
+
+
+var htmlcodes = {
+	aliceblue: 'f0f8ff',
+	antiquewhite: 'faebd7',
+	aqua: '00ffff',
+	aquamarine: '7fffd4',
+	azure: 'f0ffff',
+	beige: 'f5f5dc',
+	bisque: 'ffe4c4',
+	black: '000000',
+	blanchedalmond: 'ffebcd',
+	blue: '0000ff',
+	blueviolet: '8a2be2',
+	brown: 'a52a2a',
+	burlywood: 'deb887',
+	cadetblue: '5f9ea0',
+	chartreuse: '7fff00',
+	chocolate: 'd2691e',
+	coral: 'ff7f50',
+	cornflowerblue: '6495ed',
+	cornsilk: 'fff8dc',
+	crimson: 'dc143c',
+	cyan: '00ffff',
+	darkblue: '00008b',
+	darkcyan: '008b8b',
+	darkgoldenrod: 'b8860b',
+	darkgray: 'a9a9a9',
+	darkgreen: '006400',
+	darkkhaki: 'bdb76b',
+	darkmagenta: '8b008b',
+	darkolivegreen: '556b2f',
+	darkorange: 'ff8c00',
+	darkorchid: '9932cc',
+	darkred: '8b0000',
+	darksalmon: 'e9967a',
+	darkseagreen: '8fbc8f',
+	darkslateblue: '483d8b',
+	darkslategray: '2f4f4f',
+	darkturquoise: '00ced1',
+	darkviolet: '9400d3',
+	deeppink: 'ff1493',
+	deepskyblue: '00bfff',
+	dimgray: '696969',
+	dodgerblue: '1e90ff',
+	feldspar: 'd19275',
+	firebrick: 'b22222',
+	floralwhite: 'fffaf0',
+	forestgreen: '228b22',
+	fuchsia: 'ff00ff',
+	gainsboro: 'dcdcdc',
+	ghostwhite: 'f8f8ff',
+	gold: 'ffd700',
+	goldenrod: 'daa520',
+	gray: '808080',
+	grey: '808080',
+	green: '00ff00',
+	greenyellow: 'adff2f',
+	honeydew: 'f0fff0',
+	hotpink: 'ff69b4',
+	indianred : 'cd5c5c',
+	indigo : '4b0082',
+	ivory: 'fffff0',
+	khaki: 'f0e68c',
+	lavender: 'e6e6fa',
+	lavenderblush: 'fff0f5',
+	lawngreen: '7cfc00',
+	lemonchiffon: 'fffacd',
+	lightblue: 'add8e6',
+	lightcoral: 'f08080',
+	lightcyan: 'e0ffff',
+	lightgoldenrodyellow: 'fafad2',
+	lightgrey: 'd3d3d3',
+	lightgreen: '90ee90',
+	lightpink: 'ffb6c1',
+	lightsalmon: 'ffa07a',
+	lightseagreen: '20b2aa',
+	lightskyblue: '87cefa',
+	lightslateblue: '8470ff',
+	lightslategray: '778899',
+	lightsteelblue: 'b0c4de',
+	lightyellow: 'ffffe0',
+	lime: '00ff00',
+	limegreen: '32cd32',
+	linen: 'faf0e6',
+	magenta: 'ff00ff',
+	maroon: '800000',
+	mediumaquamarine: '66cdaa',
+	mediumblue: '0000cd',
+	mediumorchid: 'ba55d3',
+	mediumpurple: '9370d8',
+	mediumseagreen: '3cb371',
+	mediumslateblue: '7b68ee',
+	mediumspringgreen: '00fa9a',
+	mediumturquoise: '48d1cc',
+	mediumvioletred: 'c71585',
+	midnightblue: '191970',
+	mintcream: 'f5fffa',
+	mistyrose: 'ffe4e1',
+	moccasin: 'ffe4b5',
+	navajowhite: 'ffdead',
+	navy: '000080',
+	oldlace: 'fdf5e6',
+	olive: '808000',
+	olivedrab: '6b8e23',
+	orange: 'ffa500',
+	orangered: 'ff4500',
+	orchid: 'da70d6',
+	palegoldenrod: 'eee8aa',
+	palegreen: '98fb98',
+	paleturquoise: 'afeeee',
+	palevioletred: 'd87093',
+	papayawhip: 'ffefd5',
+	peachpuff: 'ffdab9',
+	peru: 'cd853f',
+	pink: 'ffc0cb',
+	plum: 'dda0dd',
+	powderblue: 'b0e0e6',
+	purple: '800080',
+	red: 'ff0000',
+	rosybrown: 'bc8f8f',
+	royalblue: '4169e1',
+	saddlebrown: '8b4513',
+	salmon: 'fa8072',
+	sandybrown: 'f4a460',
+	seagreen: '2e8b57',
+	seashell: 'fff5ee',
+	sienna: 'a0522d',
+	silver: 'c0c0c0',
+	skyblue: '87ceeb',
+	slateblue: '6a5acd',
+	slategray: '708090',
+	snow: 'fffafa',
+	springgreen: '00ff7f',
+	steelblue: '4682b4',
+	tan: 'd2b48c',
+	teal: '008080',
+	thistle: 'd8bfd8',
+	tomato: 'ff6347',
+	turquoise: '40e0d0',
+	violet: 'ee82ee',
+	violetred: 'd02090',
+	wheat: 'f5deb3',
+	white: 'ffffff',
+	whitesmoke: 'f5f5f5',
+	yellow: 'ffff00',
+	yellowgreen: '9acd32'
+};
+
+
+var colorparsers = [
+	{
+	    re: /^rgb\s*\((\d{1,3}),\s*(\d{1,3}),\s*(\d{1,3})\)$/,
+	    process: function (bits){
+	        return [
+	            parseInt(bits[1]),
+	            parseInt(bits[2]),
+	            parseInt(bits[3])
+	        ];
+	    }
+	},
+	{
+	    re: /^rgb\s+(\d{1,3})\s+(\d{1,3})\s+(\d{1,3})$/,
+	    process: function (bits) {
+	        return [
+	            parseInt(bits[1]),
+	            parseInt(bits[2]),
+	            parseInt(bits[3])
+	        ];
+	    }
+	},
+	{
+	    re: /^([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2})$/,
+	    process: function (bits) {
+	        return [
+	            parseInt(bits[1], 16),
+	            parseInt(bits[2], 16),
+	            parseInt(bits[3], 16)
+	        ];
+	    }
+	},
+	{
+	    re: /^([0-9a-fA-F]{1})([0-9a-fA-F]{1})([0-9a-fA-F]{1})$/,
+	    process: function (bits) {
+	        return [
+	            parseInt(bits[1] + bits[1], 16),
+	            parseInt(bits[2] + bits[2], 16),
+	            parseInt(bits[3] + bits[3], 16)
+	        ];
+	    }
+	},
+	{
+	    re: /^hsl\s+(\d{1,3})\s+(\d{1,3})\s+(\d{1,3})$/,
+	    process: function (bits) {
+	    	var rgb = hslToRgb(bits[1], bits[2], bits[3]);
+	        return [
+	            rgb.r,
+	            rgb.g,
+	            rgb.b
+	        ];
+	    }
+	},
+	{
+	    re: /^hsv\s+(\d{1,3})\s+(\d{1,3})\s+(\d{1,3})$/,
+	    process: function (bits) {
+	    	var rgb = hsvToRgb(bits[1], bits[2], bits[3]);
+	        return [
+	            rgb.r,
+	            rgb.g,
+	            rgb.b
+	        ];
+	    }
+	}
+];
+
+function log() { if (console) { console.log( Array.prototype.slice.call(arguments) ); } }
+
+return tc;
+
+})();
