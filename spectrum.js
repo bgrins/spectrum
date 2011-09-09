c1e8c96d0727766f24bb5af393b0fa580b6313b3
bgrins <briangrinstead@gmail.com>
2011-09-09 16:16:30 -0500

    go back to dark

--- spectrum.js
+++ spectrum.js
@@ -18,7 +18,7 @@
         hide: function() { },
         showPallet: false,
         maxPalletSize: 12,
-        theme: 'spectrum-light',
+        theme: 'spectrum-dark',
         pallet: ['white', 'black']
     },
     spectrums = [],
