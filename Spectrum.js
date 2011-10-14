75cdcc097c9cebdd7d408782128d2b13800d01d3
bgrins <briangrinstead@gmail.com>
2011-10-14 14:08:21 -0500

    code formatting to webkit style

--- Spectrum.js
+++ Spectrum.js
@@ -84,12 +84,24 @@ WebInspector.Spectrum.hsvToRgb = function(h, s, v, a) {
     var t = v * (1 - (1 - f) * s);
     
     switch(i % 6) {
-        case 0: r = v, g = t, b = p; break;
-        case 1: r = q, g = v, b = p; break;
-        case 2: r = p, g = v, b = t; break;
-        case 3: r = p, g = q, b = v; break;
-        case 4: r = t, g = p, b = v; break;
-        case 5: r = v, g = p, b = q; break;
+    case 0: 
+        r = v, g = t, b = p; 
+        break;
+    case 1: 
+        r = q, g = v, b = p; 
+        break;
+    case 2: 
+        r = p, g = v, b = t; 
+        break;
+    case 3: 
+        r = p, g = q, b = v; 
+        break;
+    case 4: 
+        r = t, g = p, b = v; 
+        break;
+    case 5: 
+        r = v, g = p, b = q; 
+        break;
     }
     
     return [r * 255, g * 255, b * 255, a];
@@ -108,13 +120,20 @@ WebInspector.Spectrum.rgbToHsv = function(r, g, b, a) {
     s = max == 0 ? 0 : d / max;
 
     if(max == min) {
-        h = 0; // achromatic
+        // achromatic
+        h = 0; 
     }
     else {
         switch(max) {
-            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
-            case g: h = (b - r) / d + 2; break;
-            case b: h = (r - g) / d + 4; break;
+        case r: 
+            h = (g - b) / d + (g < b ? 6 : 0); 
+            break;
+        case g: 
+            h = (b - r) / d + 2; 
+            break;
+        case b: 
+            h = (r - g) / d + 4; 
+            break;
         }
         h /= 6;
     }
