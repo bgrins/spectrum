7434eb77f454fb6e1faccc0e3e4fefc9f70befa2
bgrins <briangrinstead@gmail.com>
2012-10-10 14:55:18 -0500

    whitespace

--- docs/docs.js
+++ docs/docs.js
@@ -19,31 +19,31 @@ $("#full").spectrum({
         updateBorders(color);
     },
     show: function () {
-    
+
     },
     beforeShow: function () {
-    
+
     },
     hide: function (color) {
         updateBorders(color);
     },
-    
+
     palette: [
-        ["rgb(0, 0, 0)", "rgb(67, 67, 67)", "rgb(102, 102, 102)", /*"rgb(153, 153, 153)","rgb(183, 183, 183)",*/ 
+        ["rgb(0, 0, 0)", "rgb(67, 67, 67)", "rgb(102, 102, 102)", /*"rgb(153, 153, 153)","rgb(183, 183, 183)",*/
         "rgb(204, 204, 204)", "rgb(217, 217, 217)", /*"rgb(239, 239, 239)", "rgb(243, 243, 243)",*/ "rgb(255, 255, 255)"],
         ["rgb(152, 0, 0)", "rgb(255, 0, 0)", "rgb(255, 153, 0)", "rgb(255, 255, 0)", "rgb(0, 255, 0)",
-        "rgb(0, 255, 255)", "rgb(74, 134, 232)", "rgb(0, 0, 255)", "rgb(153, 0, 255)", "rgb(255, 0, 255)"], 
-        ["rgb(230, 184, 175)", "rgb(244, 204, 204)", "rgb(252, 229, 205)", "rgb(255, 242, 204)", "rgb(217, 234, 211)", 
-        "rgb(208, 224, 227)", "rgb(201, 218, 248)", "rgb(207, 226, 243)", "rgb(217, 210, 233)", "rgb(234, 209, 220)", 
-        "rgb(221, 126, 107)", "rgb(234, 153, 153)", "rgb(249, 203, 156)", "rgb(255, 229, 153)", "rgb(182, 215, 168)", 
-        "rgb(162, 196, 201)", "rgb(164, 194, 244)", "rgb(159, 197, 232)", "rgb(180, 167, 214)", "rgb(213, 166, 189)", 
-        "rgb(204, 65, 37)", "rgb(224, 102, 102)", "rgb(246, 178, 107)", "rgb(255, 217, 102)", "rgb(147, 196, 125)", 
+        "rgb(0, 255, 255)", "rgb(74, 134, 232)", "rgb(0, 0, 255)", "rgb(153, 0, 255)", "rgb(255, 0, 255)"],
+        ["rgb(230, 184, 175)", "rgb(244, 204, 204)", "rgb(252, 229, 205)", "rgb(255, 242, 204)", "rgb(217, 234, 211)",
+        "rgb(208, 224, 227)", "rgb(201, 218, 248)", "rgb(207, 226, 243)", "rgb(217, 210, 233)", "rgb(234, 209, 220)",
+        "rgb(221, 126, 107)", "rgb(234, 153, 153)", "rgb(249, 203, 156)", "rgb(255, 229, 153)", "rgb(182, 215, 168)",
+        "rgb(162, 196, 201)", "rgb(164, 194, 244)", "rgb(159, 197, 232)", "rgb(180, 167, 214)", "rgb(213, 166, 189)",
+        "rgb(204, 65, 37)", "rgb(224, 102, 102)", "rgb(246, 178, 107)", "rgb(255, 217, 102)", "rgb(147, 196, 125)",
         "rgb(118, 165, 175)", "rgb(109, 158, 235)", "rgb(111, 168, 220)", "rgb(142, 124, 195)", "rgb(194, 123, 160)",
         "rgb(166, 28, 0)", "rgb(204, 0, 0)", "rgb(230, 145, 56)", "rgb(241, 194, 50)", "rgb(106, 168, 79)",
         "rgb(69, 129, 142)", "rgb(60, 120, 216)", "rgb(61, 133, 198)", "rgb(103, 78, 167)", "rgb(166, 77, 121)",
         /*"rgb(133, 32, 12)", "rgb(153, 0, 0)", "rgb(180, 95, 6)", "rgb(191, 144, 0)", "rgb(56, 118, 29)",
         "rgb(19, 79, 92)", "rgb(17, 85, 204)", "rgb(11, 83, 148)", "rgb(53, 28, 117)", "rgb(116, 27, 71)",*/
-        "rgb(91, 15, 0)", "rgb(102, 0, 0)", "rgb(120, 63, 4)", "rgb(127, 96, 0)", "rgb(39, 78, 19)", 
+        "rgb(91, 15, 0)", "rgb(102, 0, 0)", "rgb(120, 63, 4)", "rgb(127, 96, 0)", "rgb(39, 78, 19)",
         "rgb(12, 52, 61)", "rgb(28, 69, 135)", "rgb(7, 55, 99)", "rgb(32, 18, 77)", "rgb(76, 17, 48)"]
     ]
 });
@@ -67,10 +67,10 @@ $("#pick1").spectrum({
         $("#switch-current-hex").text(hex);
     },
     show: function() {
-       
+
     },
     hide: function() {
-       
+
     },
     showInput: true,
     showPalette: true,
@@ -81,15 +81,16 @@ $("#collapsed").spectrum({
     color: "#dd3333",
     change: updateBorders,
     show: function() {
-       
+
     },
     hide: function() {
-       
+
     }
 });
 
 $("#flat").spectrum({
     flat: true,
+    showInput: true,
     move: updateBorders
 });
 
@@ -98,28 +99,28 @@ $("#showInput").spectrum({
     showInput: true,
     change: updateBorders,
     show: function() {
-       
+
     },
     hide: function() {
-       
-    } 
+
+    }
 });
 
 $("#openWithLink").spectrum({
     color: "#dd3333",
     change: updateBorders,
     show: function() {
-       
+
     },
     hide: function() {
-       
+
     }
 });
 
 $("#showPalette").spectrum({
     showPalette: true,
     palette: [
-        ['black', 'white', 'blanchedalmond'], 
+        ['black', 'white', 'blanchedalmond'],
         ['rgb(255, 128, 0);', 'hsv 100 70 50', 'lightyellow']
     ],
     change: updateBorders
@@ -131,7 +132,7 @@ $("#showPaletteOnly").spectrum({
     color: 'blanchedalmond',
     showPaletteOnly: true,
     palette: [
-        ['black', 'white', 'blanchedalmond', 
+        ['black', 'white', 'blanchedalmond',
         'rgb(255, 128, 0);', 'hsv 100 70 50'],
         ['red', 'yellow', 'green', 'blue', 'violet']
     ]
@@ -147,11 +148,11 @@ $("#showInputAndInitial").spectrum({
 });
 
 
-$("#changeOnMove").spectrum({ 
+$("#changeOnMove").spectrum({
     move: function(c) {
         var label = $("#changeOnMoveLabel");
         label.text("Move called: " + c.toHexString());
-    } 
+    }
 });
 $("#changeOnMoveNo").spectrum({
     change: function(c) {
@@ -237,7 +238,7 @@ $('#toc').toc({
     'highlightOffset': 100, //offset to trigger the next headline
     'anchorName': function(i, heading, prefix) { //custom function for anchor name
         return prefix+i;
-    } 
+    }
 });
 
 prettyPrint();
