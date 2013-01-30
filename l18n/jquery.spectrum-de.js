df12311183f8b871a0dfe5fe2af40f589d848e72
wallenium <s.wallmann@kompsoft.de>
2013-01-30 22:41:42 +0100

    Added german l18n file

---
+++ l18n/jquery.spectrum-de.js
@@ -0,0 +1,13 @@
+// Spectrum Colorpicker
+// https://github.com/bgrins/spectrum
+// Author: Brian Grinstead
+// License: MIT
+
+(function ( $ ) {
+
+  $.extend($.fn.spectrum.defaults, {
+      cancelText: "Abbrechen",
+      chooseText: "Wählen"
+  });
+
+})( jQuery );
