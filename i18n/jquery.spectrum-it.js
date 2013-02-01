7bc4eefbccf2ae9f3f12d1f7973e69aaca9b202c
wallenium <s.wallmann@kompsoft.de>
2013-02-01 14:28:25 +0100

    Dansih, Italian and Russian

---
+++ i18n/jquery.spectrum-it.js
@@ -0,0 +1,11 @@
+// Spectrum Colorpicker
+// https://github.com/bgrins/spectrum
+// Author: Brian Grinstead
+// License: MIT
+
+(function ( $ ) {
+	$.extend($.fn.spectrum.defaults, {
+		cancelText: "annulla",
+		chooseText: "scegli"
+	});
+})( jQuery );
