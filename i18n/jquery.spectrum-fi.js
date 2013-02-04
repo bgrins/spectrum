0a37a6d00c24a83dad5a94dcd347b0e0787a4b2d
bgrins <briangrinstead@gmail.com>
2013-02-04 17:36:59 -0600

    add Finnish translation

---
+++ i18n/jquery.spectrum-fi.js
@@ -0,0 +1,12 @@
+// Spectrum Colorpicker
+// Finnish (fi) localization
+// https://github.com/bgrins/spectrum
+
+(function ( $ ) {
+
+  $.extend($.fn.spectrum.defaults, {
+      cancelText: "Kumoa",
+      chooseText: "Valitse"
+  });
+
+})( jQuery );
