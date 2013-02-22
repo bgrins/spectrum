c4cc33392d2ccd0f62a20e69d98bb933515a725a
Léo Maradan <maradan.leo@gmail.com>
2013-02-22 16:55:02 +0100

    Add French and Japanese translation

---
+++ i18n/jquery.spectrum-ja.js
@@ -0,0 +1,14 @@
+// Spectrum Colorpicker
+// Japanese (fr) localization
+// https://github.com/bgrins/spectrum
+
+(function ( $ ) {
+
+    var localization = $.spectrum.localization["ja"] = {
+        cancelText: "中止",
+        chooseText: "選択"
+    };
+
+    $.extend($.fn.spectrum.defaults, localization);
+
+})( jQuery );
