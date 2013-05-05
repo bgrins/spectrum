0c924a55c0201f7d24c1ea1272d6b56d778baa45
bgrins <briangrinstead@gmail.com>
2013-05-05 14:10:18 -0500

    update log function from https://gist.github.com/bgrins/5108712

--- spectrum.js
+++ spectrum.js
@@ -101,18 +101,6 @@
         ].join("");
     })();
 
-    var log = function() { };
-    if (window.console) {
-      if (Function.prototype.bind) {
-        log = Function.prototype.bind.call(console.log, console);
-      }
-      else {
-        log = function() {
-          Function.prototype.apply.call(console.log, console, arguments);
-        };
-      }
-    }
-
     function paletteTemplate (p, color, className) {
         var html = [];
         for (var i = 0; i < p.length; i++) {
@@ -914,6 +902,8 @@
         };
     }
 
+
+    function log(){/* jshint -W021 */if(window.console){if(Function.prototype.bind)log=Function.prototype.bind.call(console.log,console);else log=function(){Function.prototype.apply.call(console.log,console,arguments);};log.apply(this,arguments);}}
 
     /**
     * Define a jQuery plugin
