2be85bb36514252207f0479d7fa94c039e77a1e9
Suleyman Melikoglu <suleymanmelikoglu@gmail.com>
2013-07-30 16:20:44 +0300

    turkish translation is added

---
+++ i18n/jquery.spectrum-tr.js
@@ -0,0 +1,14 @@
+// Spectrum Colorpicker
+// Italian (it) localization
+// https://github.com/bgrins/spectrum
+
+(function ( $ ) {
+
+    var localization = $.spectrum.localization["tr"] = {
+		cancelText: "iptal",
+		chooseText: "tamam"
+	};
+
+    $.extend($.fn.spectrum.defaults, localization);
+
+})( jQuery );
