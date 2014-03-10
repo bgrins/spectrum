926d8b2ce5fe589bbcab7bd628d4eca7cda0db1c
Steven Snoeijen <stevensnoeijen@hotmail.com>
2014-03-10 09:25:30 -0700

    added dutch translations

---
+++ i18n/jquery.spectrum-nl-nl.js
@@ -0,0 +1,14 @@
+// Spectrum Colorpicker
+// Dutch (nl-nl) localization
+// https://github.com/bgrins/spectrum
+
+(function ( $ ) {
+
+    var localization = $.spectrum.localization["nl-nl"] = {
+        cancelText: "Annuleer",
+        chooseText: "Kies"
+    };
+
+    $.extend($.fn.spectrum.defaults, localization);
+
+})( jQuery );
