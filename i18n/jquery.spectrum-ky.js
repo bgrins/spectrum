5f03efdfc9cd7cc43fb027896295e70ae6aafd3c
Sultan Imanhodjaev <sultan.imanhodjaev@gmail.com>
2014-03-17 21:48:58 +0600

    Added translation for clearText

--- i18n/jquery.spectrum-ky.js
+++ i18n/jquery.spectrum-ky.js
@@ -4,6 +4,7 @@
     var localization = $.spectrum.localization["ky"] = {
         cancelText: "баш тартуу",
         chooseText: "тандоо"
+        clearText: "Тандалган түстү жокко чыгаруу"
     };
 
     $.extend($.fn.spectrum.defaults, localization);
