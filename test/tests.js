6191938db1da62fc8b7c07d6b29593eba0b6397e
Marc-Andre Lafortune <github@marc-andre.ca>
2014-08-22 23:47:05 -0400

    Remove trailing whitespace

--- test/tests.js
+++ test/tests.js
@@ -391,30 +391,30 @@ test ("Toggle Picker Area button works as expected", function() {
     togglePaletteOnly: true,
     color: "red"
   });
-  
+
   var spectrum = el.spectrum("container").show(),
       toggle = spectrum.find(".sp-palette-toggle"),
       picker = spectrum.find(".sp-picker-container"),
       palette = spectrum.find(".sp-palette-container");
-  
+
   // Open the Colorpicker
   el.spectrum("show");
   equal(picker.is(":hidden"), true, "The picker area is hidden by default.");
   ok(spectrum.hasClass("sp-palette-only"), "The 'palette-only' class is enabled.");
-  
+
   // Click the Picker area Toggle button to show the Picker
   toggle.click();
 
   equal(picker.is(":hidden"), false, "After toggling, the picker area is visible.");
   ok(!spectrum.hasClass("sp-palette-only"), "The 'palette-only' class is disabled.");
   equal(Math.round(picker.offset().top), Math.round(palette.offset().top), "The picker area is next to the palette.");
-  
+
   // Click the toggle again to hide the picker
   toggle.trigger("click");
-  
+
   equal(picker.is(":hidden"), true, "After toggling again, the picker area is hidden.");
   ok(spectrum.hasClass("sp-palette-only"), "And the 'palette-only' class is enabled.");
-  
+
   // Cleanup
   el.spectrum("hide");
   el.spectrum("destroy");
