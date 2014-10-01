030ca2f824a6fecdec7cfb3c3d53e8ee06486b33
xenomarz <xenomarz@gmail.com>
2014-10-02 00:55:19 +0300

    Create jquery.spectrum-he.js
    Localization work - added hebrew (he) support.

---
+++ i18n/jquery.spectrum-he.js
@@ -0,0 +1,18 @@
+// Spectrum Colorpicker
+// Hebrew (he) localization
+// https://github.com/bgrins/spectrum
+
+(function ( $ ) {
+
+    var localization = $.spectrum.localization["he"] = {
+        cancelText: "בטל",
+        chooseText: "בחר",
+        clearText: "אפס בחירה",
+        noColorSelectedText: "לא נבחר צבע",
+        togglePaletteMoreText: "יותר",
+        togglePaletteLessText: "פחות"
+    };
+
+    $.extend($.fn.spectrum.defaults, localization);
+
+})( jQuery );
