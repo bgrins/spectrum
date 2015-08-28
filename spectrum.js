e674fabf80adb7674ee99ae6dd1997f70b37493f
avoliva <adamvoliva@gmail.com>
2015-08-27 20:11:20 -0700

    Actual fix for changing allowEmpty from false to true.

--- spectrum.js
+++ spectrum.js
@@ -260,7 +260,9 @@
                 }
             }
             
-            allowEmpty = opts.allowEmpty && !doc.isInputTypeColor;
+
+            isInputTypeColor = isInput && boundElement.attr("type") === "color" && inputTypeColorSupport();
+            allowEmpty = opts.allowEmpty && !isInputTypeColor;
 
             container.toggleClass("sp-flat", flat);
             container.toggleClass("sp-input-disabled", !opts.showInput);
