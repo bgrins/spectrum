1f499fd8d6dd840ba6605fab9caad98ec6b2f766
avoliva <adamvoliva@gmail.com>
2015-09-07 16:31:23 -0700

    Change the replacerClassName.

--- spectrum.js
+++ spectrum.js
@@ -271,6 +271,8 @@
             container.toggleClass("sp-initial-disabled", !opts.showInitial);
             container.addClass(opts.className).addClass(opts.containerClassName);
 
+            replacer.addClass(opts.replacerClassName);
+
             reflow();
         }
 
