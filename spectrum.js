9f9dd5d2620c9ef4974717565c5030ae10b3d7e5
Rydori95 <daniel.siegert@udo.edu>
2016-01-02 00:15:26 +0100

    Update spectrum.js

--- spectrum.js
+++ spectrum.js
@@ -1091,7 +1091,7 @@
                     maxWidth = $(element).width();
                     offset = $(element).offset();
 
-                    $(doc).bind(duringDragEvents);
+                    $(doc.body).bind(duringDragEvents);
                     $(doc.body).addClass("sp-dragging");
 
                     move(e);
