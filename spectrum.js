663826ca53182abf82c76732921a3a8928b9b290
Yves Astier <yves.astier@agence-tbd.com>
2016-03-17 16:35:40 +0100

    to avoid special case rgb(0,0,255)

--- spectrum.js
+++ spectrum.js
@@ -138,7 +138,7 @@
             var current = p[i];
             if(current) {
                 var tiny = tinycolor(current);
-                var c = tiny.toHsl().l < 0.5 ? "sp-thumb-el sp-thumb-dark" : "sp-thumb-el sp-thumb-light";
+                var c = tiny.toHsl().l <= 0.5 ? "sp-thumb-el sp-thumb-dark" : "sp-thumb-el sp-thumb-light";
                 c += (tinycolor.equals(color, current)) ? " sp-thumb-active" : "";
                 var formattedString = tiny.toString(opts.preferredFormat || "rgb");
                 var swatchStyle = rgbaSupport ? ("background-color:" + tiny.toRgbString()) : "filter:" + tiny.toFilter();
