d3f9b9283507827cc477addfdb42f718e68274a0
Oriol Jiménez <oriol.jimenez@gmail.com>
2016-06-28 11:22:11 +0200

    Catalan translation
    The javascript i18n translation for catalan language

---
+++ i18n/jquery.spectrum-ca.js
@@ -0,0 +1,14 @@
+// Spectrum Colorpicker
+// Catalan (ca) localization
+// https://github.com/bgrins/spectrum
+
+(function ( $ ) {
+
+    var localization = $.spectrum.localization["ca"] = {
+        cancelText: "Cancel·lar",
+        chooseText: "Escollir"
+    };
+
+    $.extend($.fn.spectrum.defaults, localization);
+
+})( jQuery );
