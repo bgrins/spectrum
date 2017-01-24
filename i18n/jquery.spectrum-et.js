561fbad2ae7b731038f085d8c3637aa5c06f3eec
ahke <ahti@agilist.ee>
2017-01-24 11:13:46 +0200

    Create jquery.spectrum-et.js

---
+++ i18n/jquery.spectrum-et.js
@@ -0,0 +1,18 @@
+// Spectrum Colorpicker
+// Estonian (et) localization
+// https://github.com/bgrins/spectrum
+
+(function ( $ ) {
+
+    var localization = $.spectrum.localization["et"] = {
+        cancelText: "Katkesta",
+        chooseText: "Vali",
+        clearText: "Tühista värvivalik",
+        noColorSelectedText: "Ühtki värvi pole valitud",
+        togglePaletteMoreText: "Rohkem",
+        togglePaletteLessText: "Vähem"
+    };
+
+    $.extend($.fn.spectrum.defaults, localization);
+
+})( jQuery );
