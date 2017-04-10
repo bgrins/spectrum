fea75002f77cad7e6f5b82701fe113b8f49e0efb
Vitor Barbosa <vitorhsb@gmail.com>
2017-04-10 18:38:10 +0100

    Portuguese Translation
    Add portuguese (pt-pt) translation

---
+++ i18n/jquery.spectrum-pt-pt.js
@@ -0,0 +1,18 @@
+// Spectrum Colorpicker
+// Portuguese (pt-pt) localization
+// https://github.com/bgrins/spectrum
+
+(function ( $ ) {
+
+    var localization = $.spectrum.localization["pt-pt"] = {
+        cancelText: "Cancelar",
+        chooseText: "Escolher",
+        clearText: "Limpar cor seleccionada",
+        noColorSelectedText: "Nenhuma cor seleccionada",
+        togglePaletteMoreText: "Mais",
+        togglePaletteLessText: "Menos"
+    };
+
+    $.extend($.fn.spectrum.defaults, localization);
+
+})( jQuery );
