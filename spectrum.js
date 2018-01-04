6452ac20806f34b61a12ea31fc384b24891bae79
Alexander Silberschneider <asilberschneider@users.noreply.github.com>
2018-01-04 15:38:22 +0100

    Replace deprecated event handlers
    Replace the usage of (deprecated) shortcut methods to attach event handlers with calls to "on".

--- spectrum.js
+++ spectrum.js
@@ -321,14 +321,14 @@
             }
 
             // Prevent clicks from bubbling up to document.  This would cause it to be hidden.
-            container.click(stopPropagation);
+            container.on("click", stopPropagation);
 
             // Handle user typed input
             textInput.change(setFromTextInput);
             textInput.on("paste", function () {
                 setTimeout(setFromTextInput, 1);
             });
-            textInput.keydown(function (e) { if (e.keyCode == 13) { setFromTextInput(); } });
+            textInput.on("keydown", function (e) { if (e.keyCode == 13) { setFromTextInput(); } });
 
             cancelButton.text(opts.cancelText);
             cancelButton.on("click.spectrum", function (e) {
