a67c75acb3520d5521bfb46d9e2ce9a33674c375
greendimka <dmitrij@trustbuddy.com>
2018-03-14 09:41:58 +0100

    Create jquery.spectrum-nb-no.js

---
+++ i18n/jquery.spectrum-nb-no.js
@@ -0,0 +1,18 @@
+// Spectrum Colorpicker
+// Norwegian, Bokmål (nb-no) localization
+// https://github.com/greendimka
+
+(function ( $ ) {
+
+    var localization = $.spectrum.localization["nb-no"] = {
+        cancelText: "Avbryte",
+        chooseText: "Velg",
+        clearText: "Tilbakestill",
+        noColorSelectedText: "Farge er ikke valgt",
+        togglePaletteMoreText: "Mer",
+        togglePaletteLessText: "Mindre"
+    };
+
+    $.extend($.fn.spectrum.defaults, localization);
+
+})( jQuery );
