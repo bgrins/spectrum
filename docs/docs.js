1ce7ff7bd6ee8a96c2a0c6870bc997aa35535f78
Sebastian Castro <sebastian.castro@protonmail.com>
2019-12-20 19:43:48 +0100

    Style Docs

--- docs/docs.js
+++ docs/docs.js
@@ -9,9 +9,23 @@ function updateBorders(color) {
 
 $(function() {
 
+$('pre').each(function() { hljs.highlightBlock(this) });
+
+$('#toc').toc({
+    'selectors': 'h1,h2:not(.subtitle),h3', //elements to use as headings
+    'container': '.main-container', //element to find all selectors in
+    'smoothScrolling': true, //enable or disable smooth scrolling on click
+    'prefix': 'toc', //prefix for anchor tags and class names
+    'highlightOnScroll': true, //add class to heading that is currently in focus
+    'highlightOffset': 100, //offset to trigger the next headline
+    'anchorName': function(i, heading, prefix) { //custom function for anchor name
+        return heading.id || prefix+i;
+    }
+});
+
 $("#full").spectrum({
     allowEmpty:true,
-    color: "#ECC",
+    color: "#276cb8",
     showInput: true,
     containerClassName: "full-spectrum",
     showInitial: true,
@@ -21,37 +35,6 @@ $("#full").spectrum({
     maxPaletteSize: 10,
     preferredFormat: "hex",
     localStorageKey: "spectrum.demo",
-    move: function (color) {
-        updateBorders(color);
-    },
-    show: function () {
-
-    },
-    beforeShow: function () {
-
-    },
-    hide: function (color) {
-        updateBorders(color);
-    },
-
-    palette: [
-        ["rgb(0, 0, 0)", "rgb(67, 67, 67)", "rgb(102, 102, 102)", /*"rgb(153, 153, 153)","rgb(183, 183, 183)",*/
-        "rgb(204, 204, 204)", "rgb(217, 217, 217)", /*"rgb(239, 239, 239)", "rgb(243, 243, 243)",*/ "rgb(255, 255, 255)"],
-        ["rgb(152, 0, 0)", "rgb(255, 0, 0)", "rgb(255, 153, 0)", "rgb(255, 255, 0)", "rgb(0, 255, 0)",
-        "rgb(0, 255, 255)", "rgb(74, 134, 232)", "rgb(0, 0, 255)", "rgb(153, 0, 255)", "rgb(255, 0, 255)"],
-        ["rgb(230, 184, 175)", "rgb(244, 204, 204)", "rgb(252, 229, 205)", "rgb(255, 242, 204)", "rgb(217, 234, 211)",
-        "rgb(208, 224, 227)", "rgb(201, 218, 248)", "rgb(207, 226, 243)", "rgb(217, 210, 233)", "rgb(234, 209, 220)",
-        "rgb(221, 126, 107)", "rgb(234, 153, 153)", "rgb(249, 203, 156)", "rgb(255, 229, 153)", "rgb(182, 215, 168)",
-        "rgb(162, 196, 201)", "rgb(164, 194, 244)", "rgb(159, 197, 232)", "rgb(180, 167, 214)", "rgb(213, 166, 189)",
-        "rgb(204, 65, 37)", "rgb(224, 102, 102)", "rgb(246, 178, 107)", "rgb(255, 217, 102)", "rgb(147, 196, 125)",
-        "rgb(118, 165, 175)", "rgb(109, 158, 235)", "rgb(111, 168, 220)", "rgb(142, 124, 195)", "rgb(194, 123, 160)",
-        "rgb(166, 28, 0)", "rgb(204, 0, 0)", "rgb(230, 145, 56)", "rgb(241, 194, 50)", "rgb(106, 168, 79)",
-        "rgb(69, 129, 142)", "rgb(60, 120, 216)", "rgb(61, 133, 198)", "rgb(103, 78, 167)", "rgb(166, 77, 121)",
-        /*"rgb(133, 32, 12)", "rgb(153, 0, 0)", "rgb(180, 95, 6)", "rgb(191, 144, 0)", "rgb(56, 118, 29)",
-        "rgb(19, 79, 92)", "rgb(17, 85, 204)", "rgb(11, 83, 148)", "rgb(53, 28, 117)", "rgb(116, 27, 71)",*/
-        "rgb(91, 15, 0)", "rgb(102, 0, 0)", "rgb(120, 63, 4)", "rgb(127, 96, 0)", "rgb(39, 78, 19)",
-        "rgb(12, 52, 61)", "rgb(28, 69, 135)", "rgb(7, 55, 99)", "rgb(32, 18, 77)", "rgb(76, 17, 48)"]
-    ]
 });
 
 $("#hideButtons").spectrum({
@@ -473,21 +456,5 @@ $("#btn-toggle").click(function() {
     $("#toggle").spectrum("toggle");
     return false;
 });
-
-
-$('#toc').toc({
-    'selectors': 'h2,h3', //elements to use as headings
-    'container': '#docs', //element to find all selectors in
-    'smoothScrolling': true, //enable or disable smooth scrolling on click
-    'prefix': 'toc', //prefix for anchor tags and class names
-    'highlightOnScroll': true, //add class to heading that is currently in focus
-    'highlightOffset': 100, //offset to trigger the next headline
-    'anchorName': function(i, heading, prefix) { //custom function for anchor name
-        return heading.id || prefix+i;
-    }
-});
-
-prettyPrint();
-
 
 });
