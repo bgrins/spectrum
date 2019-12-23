bf663355e7f627717f92ad0e7140c79ac5c9b327
Sebastian Castro <sebastian.castro@protonmail.com>
2019-12-23 11:28:17 +0100

    Minify css & js in dist folder, so it can be released by jsdeliver

--- Gruntfile.js
+++ Gruntfile.js
@@ -36,8 +36,19 @@ module.exports = function(grunt) {
       },
       dist: {
         files: {
-          'build/spectrum-min.js': ['spectrum.js']
+          'dist/spectrum.min.js': ['spectrum.js']
         }
+      }
+    },
+
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          src: ['spectrum.css'],
+          dest: 'dist',
+          ext: '.min.css'
+        }]
       }
     }
 
@@ -47,6 +58,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-qunit');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
 
   // Testing tasks
@@ -59,6 +71,6 @@ module.exports = function(grunt) {
   grunt.registerTask('default', ['test']);
 
   //Build Task.
-  grunt.registerTask('build', ['test', 'uglify']);
+  grunt.registerTask('build', ['test', 'uglify', 'cssmin']);
 
 };
