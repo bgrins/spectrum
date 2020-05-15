e92072cdbb868f99cbc3057cfeb2e7d689dc5422
Brian Grinstead <briangrinstead@gmail.com>
2020-05-14 19:48:22 -0700

    bump devDependencies

--- spectrum.js
+++ spectrum.js
@@ -1257,12 +1257,12 @@
         }
 
         var rgb = inputToRGB(color);
-        this._originalInput = color,
-        this._r = rgb.r,
-        this._g = rgb.g,
-        this._b = rgb.b,
-        this._a = rgb.a,
-        this._roundA = mathRound(1000 * this._a) / 1000,
+        this._originalInput = color;
+        this._r = rgb.r;
+        this._g = rgb.g;
+        this._b = rgb.b;
+        this._a = rgb.a;
+        this._roundA = mathRound(1000 * this._a) / 1000;
         this._format = opts.format || rgb.format;
         this._gradientType = opts.gradientType;
 
